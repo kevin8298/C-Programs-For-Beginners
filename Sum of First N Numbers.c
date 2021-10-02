164f279e8b9c5173d5fde478317e8918c27cdce2
kkname776 <73051069+kkname776@users.noreply.github.com>
2021-10-02 12:05:58 +0530

    Simple

--- Sum of First N Numbers.c
+++ Sum of First N Numbers.c
@@ -1,31 +1,15 @@
-#include<stdio.h>
-// declaring the recursive function
-int getSum(int);
+#include <stdio.h>
+int main() {
+    int n, i, sum = 0;
 
-int main()
-{
-    printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
-    int n, sum;
-    printf("\n\nEnter the range of n: ");
+    printf("Enter a positive integer: ");
     scanf("%d", &n);
-    sum = getSum(n);
-    printf("\n\nThe sum of first %d numbers is %d\n", n, sum);
-    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+
+    for (i = 1; i <= n; ++i) {
+        sum += i;
+    }
+
+    printf("Sum = %d", sum);
     return 0;
 }
 
-// function definition
-int getSum(int aj)
-{
-    /*
-        static variables hold their values
-        till the end of the program
-    */
-    static int sum = 0;
-    if(aj > 0)
-    {
-        sum = sum + aj;
-        getSum(aj-1);
-    }
-    return sum ;
-}
